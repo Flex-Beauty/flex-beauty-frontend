f55b85ab2066baf63d47dbf256ac11d13dafe274
Nikita Dumkin <ndumkin@mail.ru>
2024-03-10 23:45:21 +0500

    Test

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
+      HAHAHAH
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
